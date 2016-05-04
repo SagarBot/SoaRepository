6a9da0fc79139f839706e4d1311ff458411bcf3f
SagarBot <sagar01.s@gmail.com>
2016-05-04 15:19:21 +0530

    Ant Build for SOA
    Ant Build for SOA -
    ant deployComposites -verbose
    ant undeployComposites -verbose
    ant listDeployedComposites -verbose
    ant importMDSArtifacts -verbose
    ant deleteMDSArtifacts -verbose
    ant exportMDS -verbose

---
+++ BPELBuild/setEnv.cmd
@@ -0,0 +1,5 @@
+set ORACLE_HOME=C:\OraclejDEV\Middleware\jdeveloper
+set ANT_HOME=C:\OraclejDEV\Middleware\jdeveloper\ant
+set JAVA_HOME=C:\Java\jdk1.7.0_67
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%PATH%
+set CURRENT_FOLDER=%CD%
